c740dfb45757672880be694a565f65fd386ca6a4
AZIZXlaouiti <azizrocke@gmail.com>
2021-08-12 09:49:52 -0400

    completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+class Anagram
+    
+    def initialize(word)
+        @word =word
+    end
+    def match(arr)
+        match = arr.select{|e| e.chars.sort == @word.chars.sort}
+        match
+    end
+end
